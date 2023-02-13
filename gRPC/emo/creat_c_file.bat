6f983e6749ce1966a309924a08948623a030f822
dzming-git <dzm_work@163.com>
2023-02-13 16:37:57 +0800

    gRPC生成工具放置在每个功能模块外部，共同调用

--- gRPC/emo/creat_c_file.bat
+++ gRPC/emo/creat_c_file.bat
@@ -2,5 +2,5 @@ del emo_gRPC.grpc.pb.cc
 del emo_gRPC.grpc.pb.h
 del emo_gRPC.pb.cc
 del emo_gRPC.pb.h
-.\protoc.exe --grpc_out=. --plugin=protoc-gen-grpc=grpc_cpp_plugin.exe emo_gRPC.proto
-.\protoc.exe --cpp_out=. emo_gRPC.proto
+..\protoc.exe --grpc_out=. --plugin=protoc-gen-grpc=..\grpc_cpp_plugin.exe emo_gRPC.proto
+..\protoc.exe --cpp_out=. emo_gRPC.proto
--- gRPC/yolo/creat_c_file.bat
+++ gRPC/yolo/creat_c_file.bat
@@ -2,5 +2,5 @@ del yolo_gRPC.grpc.pb.cc
 del yolo_gRPC.grpc.pb.h
 del yolo_gRPC.pb.cc
 del yolo_gRPC.pb.h
-.\protoc.exe --grpc_out=. --plugin=protoc-gen-grpc=grpc_cpp_plugin.exe yolo_gRPC.proto
-.\protoc.exe --cpp_out=. yolo_gRPC.proto
+..\protoc.exe --grpc_out=. --plugin=protoc-gen-grpc=..\grpc_cpp_plugin.exe yolo_gRPC.proto
+..\protoc.exe --cpp_out=. yolo_gRPC.proto
